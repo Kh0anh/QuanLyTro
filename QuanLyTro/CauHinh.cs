c8f883eabf5f5ce5cd283115be793e1214704b21
Khanh <vedakinvn@gmail.com>
2024-12-24 22:30:54 +0700

    Initial commit

---
+++ QuanLyTro/CauHinh.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace QuanLyTro
+{
+    public static class CauHinh
+    {
+        public static readonly string TenPhanMem = "Quản Lý Trọ";
+        public static SQLiteConnection CSDL;
+
+        private static readonly string duongDanCSDL = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "database.sqlite3");
+
+        public static void init()
+        {
+            if (File.Exists(duongDanCSDL))
+            {
+                CSDL = new SQLiteConnection(string.Format("Data Source={0};Version=3;", duongDanCSDL));
+                CSDL.Open();
+
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS test(id INTEGER PRIMARY KEY, name TEXT);", CSDL))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                CSDL = new SQLiteConnection(string.Format("Data Source={0};Version=3;", duongDanCSDL));
+                CSDL.Open();
+            }
+        }
+    }
+}
---
+++ QuanLyTro/Program.cs
@@ -0,0 +1,27 @@
+﻿using QuanLyTro.Froms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTro
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            _FormMain = new FormMain();
+            Application.Run(_FormMain);
+        }
+
+        public static FormMain _FormMain { get; set; }
+    }
+}
