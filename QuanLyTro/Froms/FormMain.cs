c427281914f50f8afe2aa045569b170b7b6f6513
Huy <studywithhuyne@gmail.com>
2024-12-25 09:02:16 +0700

    Cập nhật lại uy tín cho tôi

--- QuanLyTro/Froms/FormMain.cs
+++ QuanLyTro/Froms/FormMain.cs
@@ -24,13 +24,13 @@ namespace QuanLyTro.Froms
 #if DEBUG
             ListViewItem item = new ListViewItem("PHONG-01");
             item.SubItems.Add("Thường");
-            item.SubItems.Add("2.500.000 VNĐ");
+            item.SubItems.Add("25.000.000.000 VNĐ");
             item.SubItems.Add("Thanh toán 26 ngày trước");
             item.SubItems.Add("Võ Nguyễn Gia Huy");
             item.SubItems.Add("Wifi (25.000 VNĐ), Rác (25.000 VNĐ)");
             item.SubItems.Add("2435");
             item.SubItems.Add("343");
-            item.SubItems.Add("phòng này ở hơi dơ");
+            item.SubItems.Add("Phòng này ở rất sạch, thường xuyên đậu Ferrari trước cổng và đóng tiền đúng hạn!");
             this.listView1.Items.Add(item); 
 #endif
         }
